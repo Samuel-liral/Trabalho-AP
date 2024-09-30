d35d9b3e3123120cbc368248e2652f8ac0632fe1
Samuel-liral <samuel.mendoncaliral@outlook.com>
2024-09-29 21:43:03 -0300

    main.c

--- Trabalho_AP/Desafio_Extra/main.c
+++ Trabalho_AP/Desafio_Extra/main.c
@@ -5,15 +5,15 @@ int main() {
     float N1, N2, PPD, EU, N3, notaFinal;
     int fezEU, fezN3;
 
-    
     printf("Nota da N1: ");
     scanf("%f", &N1);
-    
+
+    // Verifica se a nota da N1 está dentro do intervalo válido
     if (N1 < 0 || N1 > 4.5) {
     	
         printf("ERRO 3 \n");
         
-        return 0;
+        return 0;//Encerra o programa caso não esteja
     }
 
     printf("Nota da N2: ");
@@ -36,10 +36,11 @@ int main() {
         return 0;
     }
 
-    
+    // Pergunta se fez o exame unificado
     printf("Realizou o Exame Unificado? (1 = sim)(0 = nao)");
     scanf("%d", &fezEU);
-    
+
+    //Se sim ele recebe a nota 
     if (fezEU == 1) {
     	
         printf("Nota do Exame Unificado: ");
@@ -55,10 +56,11 @@ int main() {
         EU = 0; 
     }
 
-   
+   // Pergunta se fez a N3 
     printf("Fez a N3? (1 = sim)(0 = nao): ");
     scanf("%d", &fezN3);
-    
+
+    //Recebe a nota da N3
     if (fezN3 == 1) {
     	
         printf("Nota da N3: ");
@@ -80,16 +82,17 @@ int main() {
             N2 = N3;
         }
     }
-    
+
+    //Calculo da nota final
     notaFinal = N1 + N2 + PPD + EU;  
 
-   
+   // Se maior que 10 então a nota é 10
     if (notaFinal > 10) {
     	
         notaFinal = 10;  
     }
 
-    
+    // Imprimi a nota final e diz se foi aprovado ou não
     printf("Nota final: %.2f\n", notaFinal);
     
     if (notaFinal >= 7.0) {
